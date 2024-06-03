475868549c6ef305f8de8fff7bf46d16f32bb44e
ajim9075shaikh <142928027+ajim9075shaikh@users.noreply.github.com>
2024-06-03 08:49:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+const student = {
+    name: 'John Doe'
+};
+
+// Add a method to the Object prototype called getKeys
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
